33ad266e5463c2efabd4edf369d96f2a934571a0
zakwan1234567 <zakwankolaka@gmail.com>
2026-01-14 16:04:07 +0700

    Add files via upload

---
+++ member.js
@@ -0,0 +1,17 @@
+// ===================== FADE ANIMATION =====================
+const fades = document.querySelectorAll(".fade");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    },
+    {
+        threshold: 0.2
+    }
+);
+
+fades.forEach((fade) => observer.observe(fade));
